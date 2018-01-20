90803ffc91506a582722d38bc5bebed43e859bcc
kezian <kezian@umich.edu>
2018-01-19 22:00:56 -0500

    testing download file

--- personal/config.py
+++ personal/config.py
@@ -12,7 +12,7 @@ UPLOAD_FOLDER = os.path.join(
     os.path.dirname(os.path.dirname(os.path.realpath(__file__))),
     'var', 'uploads'
 )
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif', 'pdf'])
 MAX_CONTENT_LENGTH = 16 * 1024 * 1024
 
 # Database file is var/personal.sqlite3
--- personal/views/__init__.py
+++ personal/views/__init__.py
@@ -1,3 +1,3 @@
 """Views, one for each page."""
 from personal.views.index import index
-
+from personal.views.file import file
