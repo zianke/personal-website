3bb01b051bfe5392c02dc8d91a2547bcec070ac6
kezian <kezian@umich.edu>
2018-01-20 00:00:38 -0500

    finish about me on index page

--- personal/static/js/index.js
+++ personal/static/js/index.js
@@ -2,6 +2,9 @@
 
 
 jQuery(document).ready(function ($) {
+    /*---------------------------------------------*
+     * Darken and lighten each section
+     ---------------------------------------------*/
     var windowHeight = $(window).height(),
         gridMiddle = windowHeight * .5;
 
@@ -27,6 +30,15 @@ jQuery(document).ready(function ($) {
                 $(this).stop().fadeTo('slow', 0.4);
             }
         });
+    });
 
+
+    /*---------------------------------------------*
+     * Underline text when hovering
+     ---------------------------------------------*/
+    $('.home-content h1 span').hover(function () {
+       $(this).parents('.home-text').find('h1 span, p span').css({'text-decoration':'underline','background': 'rgba(0, 0, 0, .5)'});
+    },function () {
+        $(this).parents('.home-text').find('h1 span, p span').css({'text-decoration':'none','background': 'rgba(0, 0, 0, .3)'});
     });
 });
