422a3d68247ab14d03cf40611fa4ab1c82a5e0e9
Zian Ke <kezian@umich.edu>
2018-05-05 01:40:16 -0400

    prepend line to active nav

--- new/js/common.js
+++ new/js/common.js
@@ -1,5 +1,10 @@
-$(window).on('load', function() { // makes sure the whole site is loaded
+$(window).on('load', function () { // makes sure the whole site is loaded
     $('#status').fadeOut(); // will first fade out the loading animation
     $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
-    $('body').delay(350).css({'overflow':'visible'});
-})
+    $('body').delay(350).css({'overflow': 'visible'});
+});
+
+$(document).ready(function () {
+    $('.navs-div a[href="' + window.location.pathname + '"').addClass('navs-active')
+        .prepend('<span style="display: inline-block; transform: scale(2,1)">—&nbsp;</span>');
+});
