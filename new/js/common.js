c3d127834fdb4142e13260bc0087f0250777f823
Zian Ke <kezian@umich.edu>
2018-05-06 05:39:27 -0400

    finish auto scrolling

--- new/js/common.js
+++ new/js/common.js
@@ -5,8 +5,10 @@ $(window).on('load', function () { // makes sure the whole site is loaded
 });
 
 $(document).ready(function () {
-    $('.navs-div a[href="' + window.location.pathname + '"').addClass('navs-active')
-        .prepend('<span style="display: inline-block; transform: scale(2,1)">—&nbsp;</span>');
+    $('.navs-div a[href="' + window.location.pathname + '"').css({
+        'color': '#ffffff',
+        'padding-left': '10px'
+    }).prepend('<span style="display: inline-block; transform: scale(2,1)">—&nbsp;</span>');
 
     $('.navbar-toggle').click(function () {
         $(this).toggleClass('navbar-toggle-cancel');
--- new/js/index.js
+++ new/js/index.js
@@ -1,3 +1,55 @@
-$(document).ready(function () {
+var autoScrolling = false;
 
+$(document).ready(function () {
+    $('.navs').removeClass('navs-hover');
+    $('.navs').hover(function () {
+        autoScrolling = false;
+        $('html, body').stop().animate({
+            scrollTop: $('#' + $(this).html().toLowerCase()).offset().top
+        }, 500);
+    }, function () {
+    });
+
+    var timer;
+    var prevScrollTop = $(document).scrollTop();
+    $(window).scroll(function () {
+        if (!autoScrolling) {
+            clearInterval(timer);
+            var currentScrollTop = $(document).scrollTop();
+            console.log(prevScrollTop + ' ' + currentScrollTop);
+            timer = setTimeout(scrollToElement, 100, prevScrollTop, currentScrollTop);
+            prevScrollTop = currentScrollTop;
+        }
+    });
 });
+
+function scrollToElement(prevScrollTop, currentScrollTop) {
+    $('.navs').removeClass('navs-active');
+    console.log(': ' + prevScrollTop + ' ' + currentScrollTop);
+    var scrollTopId = nextScrollTopId(prevScrollTop, currentScrollTop);
+    autoScrolling = true;
+    $('html, body').stop().animate({
+        scrollTop: $('#' + scrollTopId).offset().top
+    }, 500, function () {
+        autoScrolling = false;
+    });
+    $('#' + scrollTopId + '-link').addClass('navs-active');
+}
+
+function nextScrollTopId(prevScrollTop, currentScrollTop) {
+    var scrollTopIds = ['home', 'biography', 'projects', 'blog', 'contact'];
+    var scrollTops = [$('#home').offset().top, $('#biography').offset().top, $('#projects').offset().top, $('#blog').offset().top, $('#contact').offset().top,]
+    if (currentScrollTop < prevScrollTop) {
+        for (var i = scrollTops.length - 1; i >= 0; i--) {
+            if (scrollTops[i] <= currentScrollTop) {
+                return scrollTopIds[i];
+            }
+        }
+    } else {
+        for (var i = 0; i < scrollTops.length; i++) {
+            if (scrollTops[i] >= currentScrollTop) {
+                return scrollTopIds[i];
+            }
+        }
+    }
+}
