8766cc590983fb40095ad60b1200bd33368a50da
kezian <kezian@umich.edu>
2018-05-08 21:39:09 -0400

    finish progress bar template

--- new/js/biography.js
+++ new/js/biography.js
@@ -1,3 +1,7 @@
 $(document).ready(function () {
     $('.biography-container').height($('.biography').height());
+
+    $(document.body).on('appear', '#appear-test', function() {
+        console.log('appear');
+    });
 });
