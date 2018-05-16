5778bd8826fd331f81d175bb88e19a98ed675dff
kezian <kezian@umich.edu>
2018-05-15 22:59:12 -0400

    test jquery

--- src/main/resources/static/js/navbar_index.js
+++ src/main/resources/static/js/navbar_index.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
-    $('.navs-div a[href="/' + ($('body').attr('id') === 'index' ? '' : ($('body').attr('id'))) + '"').css({
-        'color': '#ffffff',
-        'padding-left': '10px'
-    }).prepend('<span style="display: inline-block; transform: scale(2,1)">—&nbsp;</span>');
+    // $('.navs-div a[href="/' + ($('body').attr('id') === 'index' ? '' : ($('body').attr('id'))) + '"').css({
+    //     'color': '#ffffff',
+    //     'padding-left': '10px'
+    // }).prepend('<span style="display: inline-block; transform: scale(2,1)">—&nbsp;</span>');
 
     $('.navbar-toggle').click(function () {
         $(this).toggleClass('navbar-toggle-cancel');
