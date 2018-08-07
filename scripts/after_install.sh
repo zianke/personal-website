865802d86014bb52e35f2c3903cbd75ca52e691e
Zian Ke <zian@mpowerfinancing.com>
2018-08-07 08:51:14 -0400

    upload jar

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-./gradlew build
 kill $(ps aux | grep 'java -jar build' | awk '{print $2}')
-nohup java -jar build/libs/zian.ke-0.0.1-SNAPSHOT.jar & 
+nohup java -jar "build/libs/zian.ke-0.0.1-SNAPSHOT.jar" 
