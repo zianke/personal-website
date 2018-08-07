57a19f615357ed86abebf769b720e3d23c6b5977
Zian Ke <kezian@umich.edu>
2018-08-06 22:12:05 -0400

    upload jar

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 touch /home/ubuntu/mark2
 cd /home/ubuntu/zian.ke-test
-./gradlew build
 kill $(ps aux | grep 'java -jar build' | awk '{print $2}')
 nohup java -jar build/libs/zian.ke-0.0.1-SNAPSHOT.jar & 
