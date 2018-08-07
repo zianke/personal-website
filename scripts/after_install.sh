d7bf07cc0320dfad887a00b3cbb070e8397968b9
Zian Ke <zian@mpowerfinancing.com>
2018-08-07 09:31:56 -0400

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/home/ubuntu/zian.ke/gradlew build
+./gradlew build
 kill $(ps aux | grep 'java -jar' | awk '{print $2}')
 nohup java -jar "/home/ubuntu/zian.ke/build/libs/zian.ke-0.0.1-SNAPSHOT.jar" & 
 touch /home/ubuntu/mark3
