83b7d193f6daad4440bade1501fbc49ff479f963
Zian Ke <zian@mpowerfinancing.com>
2018-08-07 09:08:45 -0400

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 kill $(ps aux | grep 'java -jar build' | awk '{print $2}')
-nohup java -jar "build/libs/zian.ke-0.0.1-SNAPSHOT.jar" 
+nohup java -jar "build/libs/zian.ke-0.0.1-SNAPSHOT.jar" &  
