44679f4752572b6acf871190a8be316d4bd955b0
Zian Ke <kezian@umich.edu>
2018-08-09 00:47:12 -0400

    update nginx config

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,4 +2,6 @@
 cd /home/ubuntu/zian.ke
 ./gradlew build
 kill $(ps aux | grep 'java -jar' | awk '{print $2}')
-java -jar "/home/ubuntu/zian.ke/build/libs/zian.ke-0.0.1-SNAPSHOT.jar" > /dev/null 2>&1 & 
+java -jar "/home/ubuntu/zian.ke/build/libs/zian.ke-0.0.1-SNAPSHOT.jar" > /dev/null 2>&1 &
+sudo cat nginx.conf > /etc/nginx/sites-available/default
+sudo service nginx restart
