55ca6878db0a9039ad93c5dacea3e6cbbd27348a
Zian Ke <kezian@umich.edu>
2018-08-09 01:54:13 -0400

    modify url route

--- src/main/java/com/springmvc/springmongodbweb/controllers/BlogController.java
+++ src/main/java/com/springmvc/springmongodbweb/controllers/BlogController.java
@@ -90,11 +90,4 @@ public class BlogController {
         model.addAttribute("posts", posts);
         return "blog";
     }
-
-//    @GetMapping("/greeting")
-//    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-//        model.addAttribute("name", name);
-//        return "greeting";
-//    }
-
 }
